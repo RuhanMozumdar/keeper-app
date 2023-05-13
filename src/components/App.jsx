e0250309ba470567140db400056694e46837ed08
ruhan <ruhanmozumdar369@gmail.com>
2023-05-14 00:50:12 +0530

    keeper app project part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import notes from '../notes';
+import Header from './Header'
+import Footer from "./Footer";
+import Note from './Note';
 
-function App(){
-    return <div>
-        <h1>hello</h1>
+// function createNotes(noteItem) {
+//   return <Note title={noteItem.title} content={noteItem.content} />;
+// }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map( noteItem => <Note title={noteItem.title} content={noteItem.content} />)}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./components/App"
-import Header from './components/Header';
-import Footer from "./components/Footer";
-import Note from "./components/Note";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Note />
-    <Footer />
+  <App />
   </React.StrictMode>
 );
 
